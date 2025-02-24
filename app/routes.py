b38515afb4f7e626203262eb0a7f15476e943f19
aaronhegarty <122467042@umail.ucc.ie>
2025-02-24 17:00:04 +0000

    Changes made to display crests

--- app/routes.py
+++ app/routes.py
@@ -104,30 +104,8 @@ async def single_result(match_id):
         if match:
             match_players = await understat.get_match_players(match_id)
             match_shots = await understat.get_match_shots(match_id)
-            
-            # for table popup gonna do
-            team_crests = { 
-                "Arsenal": "/static/Arsenal.png",
-                "Aston Villa": "/static/AstonVila.png",
-                "Bournemouth": "/static/Bournemouth.png",
-                "Brentford": "/static/Brentford.png",
-                "Brighton": "/static/Brighton.png",
-                "Chelsea": "/static/Chelsea.png",
-                "Manchester City": "/static/City.png",
-                "Manchester United": "/static/United.png",
-                "Everton": "/static/City.png",
-                "Fulham": "/static/Fulham.png",
-                "Ipswich": "/static/Ipswich.png",
-                "Leicester": "/static/Leicester.png",
-                "Liverpool": "/static/Liverpool.png",
-                "Newcastle": "/static/Newcastle.png",
-                "Crystal Palace": "/static/Palace.png",
-                "Southampton": "/static/Southampton.png",
-                "Tottenham Hotspurs": "/static/Spurs.png",
-                "West Ham": "/static/WestHam.png",
-                "Wolverhampton Wanderers": "/static/Wolves.png",
 
-            }
+            # for table popup gonna do
             
             home_stats = {
                 "shots": len(match_shots["h"]),
@@ -146,9 +124,6 @@ async def single_result(match_id):
                 "xG": float(match["xG"]["a"]),
                 "player_stats": match_players["a"]
             }
-
-
-
                 
             return render_template(
                 "singleResult.html",
--- app/static/assignment.js
+++ app/static/assignment.js
@@ -29,3 +29,20 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
     });
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    function formatTeamName(teamName) {
+        return teamName.toLowerCase().replace(/\s/g, "_") + ".png";
+    }
+    document.querySelectorAll(".teams-container [data-team]").forEach(teamElement =>{
+        let teamName = teamElement.dataset.team;
+        let crestImg = teamElement.querySelector(".team-crest");
+
+        if(teamName){
+            let crestPath = '/static/${formatTeamName(teamName)}';
+            crestImg.src = crestPath; 
+        }
+
+    });
+});
